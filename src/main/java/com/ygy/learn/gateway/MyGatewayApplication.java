a67eaa4b19a786402a37e9c8f9a34e909902cd2c
yanguangyuan <1847344456@qq.com>
2020-01-17 17:12:49 +0800

    瞎搞

--- src/main/java/com/ygy/learn/gateway/MyGatewayApplication.java
+++ src/main/java/com/ygy/learn/gateway/MyGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author yanguangyuan
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MyGatewayApplication {
 
     public static void main(String[] args) {
