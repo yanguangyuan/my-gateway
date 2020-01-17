0ab9bbea08a9355b5166bb2d05ad492973c05b8f
yanguangyuan <1847344456@qq.com>
2020-01-17 16:57:37 +0800

    Initial commit

---
+++ src/main/java/com/ygy/learn/gateway/MyGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ygy.learn.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yanguangyuan
+ */
+@SpringBootApplication
+public class MyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGatewayApplication.class, args);
+    }
+
+}
